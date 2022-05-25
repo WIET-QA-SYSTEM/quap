3802dcb44ac40f0d6078b6c140954de713423087
Mykola Haltiuk <kaljandlux@gmail.com>
2022-05-25 15:22:36 +0200

    working

--- website/interface.py
+++ website/interface.py
@@ -2,7 +2,7 @@ import logging
 import streamlit as st
 import os
 
-from qa.dataset import UploadedDataset
+from qa.dataset import PrefetchedDataset, UploadedDataset
 from qa.system import QASystem
 
 
@@ -21,13 +21,22 @@ def get_answer_prefetched(model_name: str, prefetched_dataset_name: str, questio
     logging.info(f"""Requested answer with model {model_name}
                 on prefetched dataset {prefetched_dataset_name}:
                 {question}""")
-    pass
+    qa = QASystem(model_name)
+    dataset = PrefetchedDataset(prefetched_dataset_name)
+
+    answers = qa.ask(dataset, question)
+
+    return answers[0].answer
 
 def get_evaluation_function(model_name: str, prefetched_dataset_name: str) -> callable:
     logging.info(f"""Requested evaluation with model {model_name}
             on prefetched dataset {prefetched_dataset_name}.""")
         
-    eval = {'Retriever': {'recall_multi_hit': 1.0, 'recall_single_hit': 1.0, 'precision': 0.30999999999999994, 'map': 0.8806947200176367, 'mrr': 0.925, 'ndcg': 0.9277974990491151}, 'Reader': {'f1': 0.8366666666666667, 'exact_match': 0.75}}
+    # eval = {'Retriever': {'recall_multi_hit': 1.0, 'recall_single_hit': 1.0, 'precision': 0.30999999999999994, 'map': 0.8806947200176367, 'mrr': 0.925, 'ndcg': 0.9277974990491151}, 'Reader': {'f1': 0.8366666666666667, 'exact_match': 0.75}}
+    qa = QASystem(model_name)
+    dataset = PrefetchedDataset(prefetched_dataset_name)
+    
+    eval = qa.evaluate_reader(dataset)
     
     eval_names = {
         'recall_multi_hit': 'Multi-hit recall',
--- website/mockups.py
+++ website/mockups.py
@@ -2,13 +2,12 @@ from typing import List
 
 def get_models_list() -> List[str]:  
     return [
-        "deepset/roberta-base-squad2"
+        "deepset/roberta-base-squad2",
+        "deepset/minilm-uncased-squad2",
+        "ahotrod/albert_xxlargev1_squad2_512"
     ]
 
 def get_dataset_list() -> List[str]:
     return [
-        "Dataset 1",
-        "Dataset 2",
-        "Dataset 3",
-        "Dataset 4"
+        "squad"
     ]
